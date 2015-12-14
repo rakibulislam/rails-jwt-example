dbcf2ab66269c1c24e54e8f092e4ad3096951e66
David Shvimer <dshvimer@gmail.com>
2015-12-14 16:52:58 -0500

    removed pry

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,6 @@ class UserMailer < BaseMandrillMailer
   def send_password_reset(user_email, new_password)
 
     subject = "Password Reset"
-    binding.pry
     send_mail(user_email, subject, new_password)
   end
 end
